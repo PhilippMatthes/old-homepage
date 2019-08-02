99d2a1ebc68a5620c2a95bb0c4dd496d9a4e7bce
PhilippMatthes <github@philippmatth.es>
2019-08-02 18:49:03 +0200

    Improve timeline

--- homepage/timeline/models.py
+++ homepage/timeline/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
+
 class Milestone(models.Model):
     title = models.TextField()
     description = models.TextField()
-    date = models.DateField()
+    start_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
 
     class Meta:
-        ordering = ["date"]
+        ordering = ["-start_date"]
 
     def __str__(self):
         return self.title
