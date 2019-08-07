020e060480fb33ee12fe6177b7ea9ef06de5fedd
PhilippMatthes <github@philippmatth.es>
2019-08-07 09:09:10 +0200

    Add filesystem post delete listener to forecasts

--- homepage/siri/models.py
+++ homepage/siri/models.py
@@ -1,4 +1,8 @@
+import os
+
 from django.db import models
+from django.dispatch import receiver
+
 
 class Forecast(models.Model):
     text = models.TextField()
@@ -8,3 +12,14 @@ class Forecast(models.Model):
 
     class Meta:
         ordering = ["timestamp"]
+
+
+@receiver(models.signals.post_delete, sender=Forecast)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    """
+    Deletes file from filesystem
+    when corresponding `Forecast` object is deleted.
+    """
+    if instance.audio:
+        if os.path.isfile(instance.audio.path):
+            os.remove(instance.audio.path)
