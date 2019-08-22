4a4b5ba46267f7906281f0273f8b9f95cc641fd6
PhilippMatthes <github@philippmatth.es>
2019-08-22 20:10:44 +0200

    Add cool video as header

--- homepage/art/views.py
+++ homepage/art/views.py
@@ -1,15 +1,11 @@
-import mimetypes
-import os
-import re
-from wsgiref.util import FileWrapper
 
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.template.loader import render_to_string
-from django.http import StreamingHttpResponse
 
 from art.models import Artwork, Video
+from ui.views import stream_video_url
 
 
 def get_artwork(request):
@@ -52,65 +48,7 @@ def delete_artwork(request):
     return JsonResponse({"success": True})
 
 
-range_re = re.compile(r'bytes\s*=\s*(\d+)\s*-\s*(\d*)', re.I)
-
-
-class RangeFileWrapper(object):
-    def __init__(self, filelike, blksize=8192, offset=0, length=None):
-        self.filelike = filelike
-        self.filelike.seek(offset, os.SEEK_SET)
-        self.remaining = length
-        self.blksize = blksize
-
-    def close(self):
-        if hasattr(self.filelike, 'close'):
-            self.filelike.close()
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.remaining is None:
-            # If remaining is None, we're reading the entire file.
-            data = self.filelike.read(self.blksize)
-            if data:
-                return data
-            raise StopIteration()
-        else:
-            if self.remaining <= 0:
-                raise StopIteration()
-            data = self.filelike.read(min(self.remaining, self.blksize))
-            if not data:
-                raise StopIteration()
-            self.remaining -= len(data)
-            return data
-
-
 def stream_video(request, pk):
     video = get_object_or_404(Video, pk=pk)
-    range_header = request.META.get('HTTP_RANGE', '').strip()
-    range_match = range_re.match(range_header)
-    size = os.path.getsize(video.file.path)
-    content_type, encoding = mimetypes.guess_type(video.file.path)
-    content_type = content_type or 'application/octet-stream'
-    if range_match:
-        first_byte, last_byte = range_match.groups()
-        first_byte = int(first_byte) if first_byte else 0
-        last_byte = int(last_byte) if last_byte else size - 1
-        if last_byte >= size:
-            last_byte = size - 1
-        length = last_byte - first_byte + 1
-        resp = StreamingHttpResponse(RangeFileWrapper(open(video.file.path, 'rb'),
-                                                      offset=first_byte,
-                                                      length=length),
-                                     status=206,
-                                     content_type=content_type)
-        resp['Content-Length'] = str(length)
-        resp['Content-Range'] = 'bytes %s-%s/%s' % (first_byte, last_byte, size)
-    else:
-        resp = StreamingHttpResponse(FileWrapper(open(video.file.path, 'rb')),
-                                     content_type=content_type)
-        resp['Content-Length'] = str(size)
-    resp['Accept-Ranges'] = 'bytes'
-    return resp
+    return stream_video_url(request, video.file.path)
 
